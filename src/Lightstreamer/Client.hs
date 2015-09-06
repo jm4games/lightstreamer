f537ad79d39e9e7877afff703a7c643efb5d6d66
Joshua Montgomery <jm4games@live.com>
2015-09-05 18:00:27 -0700

    Added work for creating socket connection.

--- src/Lightstreamer/Client.hs
+++ src/Lightstreamer/Client.hs
@@ -1,12 +1,23 @@
 module Lightstreamer.Client where
 
+import Network.HTTP (sendHTTP)
+import Network.HTTP.Base (RequestMethod(POST), Response(..), mkRequest)
+import Network.Stream (ConnError(..))
+import Network.TCP (HandleStream, socketConnection)
+import Network.URI (URI(..))
+
 import qualified Data.ByteString as BS
+
+import qualified Network.Socket as S
 
 data StreamRequest = StreamRequest
     { srAdapterSet :: BS.ByteString
     , srConnectionMode :: Either KeepAliveMode PollingMode
     , srContentLength :: Maybe Int
+    , srHost :: String 
     , srPassword :: BS.ByteString
+    , srPath :: String
+    , srPort :: Int
     , srRequestedMaxBandwidth :: Maybe Int
     , srReportInfo :: Maybe Bool
     , srUser :: BS.ByteString
@@ -27,14 +38,45 @@ data StreamInfo = StreamInfo
     , requestLimit :: Maybe Int
     , serverName :: Maybe BS.ByteString
     , sessionId :: BS.ByteString
-    }
+    } deriving Show
 
 data StreamConnection = StreamConnection
     { streamInfo :: StreamInfo
     }
 
-newStreamConnection :: StreamRequest -> Either String StreamConnection
-newStreamConnection = undefined
+newStreamConnection :: StreamRequest -> IO (Either String StreamInfo)
+newStreamConnection req = do 
+    addrInfos <- S.getAddrInfo (Just addrInfo) (Just $ srHost req) (Just . show $ srPort req)
+    case addrInfos of
+      [] -> return $ Left "Failed to get host address information"
+      (a:_) -> do
+          sock <- S.socket (S.addrFamily a) S.Stream S.defaultProtocol
+          S.connect sock (S.addrAddress a)
+          conn <- socketConnection (srHost req) (srPort req) sock :: IO (HandleStream BS.ByteString)
+          result <- sendHTTP conn (mkRequest POST reqUri)
+          case result of
+            Left x -> return $
+              case x of
+                ErrorReset -> Left "Connection Reset."
+                ErrorClosed -> Left "Connection Closed."
+                ErrorParse err -> Left err
+                ErrorMisc err -> Left err
+            Right res -> 
+              case rspCode res of
+                (2,0,_) -> do
+                    putStrLn "Connected!"
+                    putStrLn (show res) 
+                    return $ Right StreamInfo {}
+                _ -> return . Left $ rspReason res
+    where 
+        addrInfo = S.defaultHints { S.addrFamily = S.AF_UNSPEC, S.addrSocketType = S.Stream }
+        reqUri = URI
+            { uriScheme = "http:"
+            , uriAuthority = Nothing
+            , uriPath = "/lightstreamer/create_session.txt"
+            , uriQuery = "?LS_op2=create&LS_cause=new.api&LS_polling=true&LS_polling_millis=0&LS_idle_millis=0&LS_cid=pcYgxn8m8%20feOojyA1S681l3g2.pz478mEb&LS_adapter_set=DynaGrid"
+            , uriFragment = ""
+            }
 
 data BindRequest = BindRequest
     { brConnectionMode :: Either KeepAliveMode PollingMode
